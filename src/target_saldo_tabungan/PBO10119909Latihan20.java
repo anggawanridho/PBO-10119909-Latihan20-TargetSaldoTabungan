730a172d26c4e508796ac920ac0f03b00bcb628e
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-28 13:03:06 +0700

    Add files via upload

---
+++ src/target_saldo_tabungan/PBO10119909Latihan20.java
@@ -0,0 +1,22 @@
+package target_saldo_tabungan;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+
+public class PBO10119909Latihan20 {
+
+    public static void main(String[] args) {
+        double saldo_awal = 3500000;
+        double i = 0;
+        do {
+            i += 1;
+            saldo_awal += Math.floor(saldo_awal * 0.08);
+            System.out.printf("Saldo di bulan ke- %1$.0f Rp. %2$,.0f%n" , i , saldo_awal);
+        } while (saldo_awal<6000000);
+    }
+}
